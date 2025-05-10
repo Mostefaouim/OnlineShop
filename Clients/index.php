f34882d2cb31f98f3f02459d624ae77f31470b48
Mostefaouim <mohammedmostefaoui2@gmail.com>
2025-05-10 22:00:31 +0200

    Update page title to Shopora

--- Clients/index.php
+++ Clients/index.php
@@ -60,7 +60,7 @@ if (isset($_GET['delete_all'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shopping Goddess</title>
+    <title>Shopora</title>
 
     <!-- custom css file link  -->
     <link rel="stylesheet" href="../css/index.css">
